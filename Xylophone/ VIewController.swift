5fecc5b1ef1aaaefbc17e4e17162311a78515fe8
Andrew Brunner <andrew.brunner@froedtert.com>
2019-03-08 13:31:08 -0600

    initial commit

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,10 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
+import AudioToolbox
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-
+    
+    var audioPlayer : AVAudioPlayer!
+    //var selectedSoundFileName : String = ""
+    let soundArray = ["note1","note2","note3","note4","note5","note6","note7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,11 +25,47 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
+        playSound(soundFileName: soundArray[sender.tag - 1])
         
+        }
+    
+    func playSound(soundFileName : String) {
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            
+        }
+        catch {
+            print(error)
+        }
+        audioPlayer.play()
+    }
+//        if let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav") {
+//            var mySound: SystemSoundID = 0
+//            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+//            AudioServicesPlaySystemSound(mySound);
+//        }
+        
+        
+        
+        //let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+        
+        
+//        do {
+//            player = try AVAudioPlayer(contentsOf : url)
+//            guard let player = player else {return}
+//
+//            player.prepareToPlay()
+//            player.play()
+//
+//        } catch let error as Error {
+//            print(error.localizedDescription)
+//        }
         
     }
     
   
 
-}
+
 
